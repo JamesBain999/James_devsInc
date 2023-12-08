72987bbe73a8dec3b5c5d898aa846d5ff466ee99
JamesBain999 <baino999@gmail.com>
2023-12-08 15:53:17 +0800

    Added text to result

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function diceValue() {
     var sel = document.getElementById('roll');
     var option = sel.options[sel.selectedIndex].value;
-    document.getElementById("result").textContent = Math.floor((Math.random() * (option)) + 1)
+    document.getElementById("result").textContent = ("Your result is: " + Math.floor((Math.random() * (option)) + 1))
 }
